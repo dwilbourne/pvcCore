05f792839785c71d7b42a5aa0f15289a7bb1ba85
Doug Wilbourne <dougwilbourne@gmail.com>
2020-07-01 03:19:15 -0400

    add ValidatorAlwaysTrue

---
+++ src/validator/base/dflt/ValidatorAlwaysTrue.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ * @version 1.0
+ */
+
+namespace pvc\validator\base\dflt;
+
+
+use pvc\msg\UserMsgInterface;
+use pvc\validator\base\ValidatorInterface;
+
+/**
+ * Class ValidatorAlwaysTrue
+ * @package pvc\validator\base\dflt
+ */
+class ValidatorAlwaysTrue implements ValidatorInterface
+{
+
+    public function validate($data): bool
+    {
+        return true;
+    }
+
+    public function getErrMsg(): ?UserMsgInterface
+    {
+        return null;
+    }
+}
